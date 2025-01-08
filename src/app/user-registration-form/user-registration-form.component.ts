e530f553230b1db970021aba346761be2b6178aa
Kari <kari.lynn.dreyer@gmail.com>
2025-01-08 10:35:36 +0100

    Fix: Adjust snackBar.open function for correct rendering

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -29,12 +29,12 @@ registerUser(): void {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      console.log(result); 
-     this.snackBar.open(result, 'OK', {
+     this.snackBar.open('Signup successful', 'OK', {
         duration: 2000
      });
     }, (result) => {
       console.log(result);
-      this.snackBar.open(result, 'OK', {
+      this.snackBar.open('Signup successful', 'OK', {
         duration: 2000
       });
     });
