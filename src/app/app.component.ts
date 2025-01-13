26c6e78f23f9f34910a02c082682ce8e9255a30a
Kari <kari.lynn.dreyer@gmail.com>
2025-01-13 10:53:15 +0100

    Remove code doubled from welcome component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-
 
 @Component({
   selector: 'app-root',
@@ -14,29 +8,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-
-  // This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
-
-  // This is the function that will open the dialog when the login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
-  // This is the function that will open the dialog when the movies button is clicked
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      // Assigning the dialog a width
-      width: '500px'
-    });
-  }
 }
