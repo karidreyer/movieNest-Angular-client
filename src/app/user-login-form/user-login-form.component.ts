003bc87f954a46422f0de098ad489d7ff10e0077
Kari <kari.lynn.dreyer@gmail.com>
2025-01-14 10:04:32 +0100

    Fix: Set the JWT token for API endpoint to retrieve

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -31,6 +31,7 @@ loginUser(): void {
   // Logic for a successful user login goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success
      console.log(result); 
+     localStorage.setItem('token', result.token);
      this.snackBar.open('Login successful', 'OK', {
         duration: 2000
      });
